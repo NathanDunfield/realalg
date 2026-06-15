0c95c94e86e1f34d10ee0ac0ea747ed4913ea4e9
Nathan Dunfield <nathan@dunfield.info>
2026-06-15 10:52:44 -0500

    Big speedup by caching BaseRealAlgebraic.inverval

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def readme():
 
 setup(
     name='realalg',
-    version='0.3.7',
+    version='0.3.8',
     description='For manipulating real algebraic numbers',
     long_description=readme(),
     author='Mark Bell',
